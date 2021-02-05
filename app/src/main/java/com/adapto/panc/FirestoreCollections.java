e646c8ecf3fdcac1a27b17531aa0a8b84453e38b
Italo Luis <isantos@fapex.org.br>
2021-02-05 17:29:30 -0300

    Criação de classe para organização das collections do Firestore

---
+++ app/src/main/java/com/adapto/panc/FirestoreCollections.java
@@ -0,0 +1,25 @@
+package com.adapto.panc;
+
+public class FirestoreCollections {
+
+    public static final String VitrineProdutosCOLLECTION = "VitrineProdutos";
+    public static final String UsuariosCOLLECTION = "Usuarios";
+    public static final String PostagensForumPANCCOLLECTION = "PostagensForumPANC";
+    public static final String EQUIPECOLLECTION = "EQUIPE";
+
+    public String getVitrineProdutosCOLLECTION() {
+        return VitrineProdutosCOLLECTION ;
+    }
+
+    public String getUsuariosCOLLECTION() {
+        return UsuariosCOLLECTION ;
+    }
+
+    public String getPostagensForumPANCCOLLECTION() {
+        return PostagensForumPANCCOLLECTION ;
+    }
+
+    public String getEQUIPECOLLECTION() {
+        return EQUIPECOLLECTION ;
+    }
+}
